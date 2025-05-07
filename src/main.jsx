7fc9866b7afe2df97a49c977199150ccccd56579
Pietra <pgodoifelipe@gmail.com>
2025-05-07 14:07:56 -0300

    mudanças na modal

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React, { createContext } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
@@ -9,7 +10,9 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import App from "./App.jsx";
 import Login from "./pages/Login.jsx";
 import Checkout from "./pages/Checkout.jsx";
+import Perfil from "./pages/Perfil.jsx";
 import { formatarMoeda } from "./utils/formatters.js";
+import CriarConta from "./pages/CriarConta.jsx";
 
 // Contexto global acessível a todas as rotas
 export const GlobalContext = createContext(null);
@@ -22,6 +25,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           <Route path="/" element={<App />} />
           <Route path="/login" element={<Login />} />
           <Route path="/checkout" element={<Checkout />} />
+          <Route path="/perfil" element={<Perfil />} />
+          <Route path="/CriarConta" element={<CriarConta/>} />
+
         </Routes>
       </BrowserRouter>
     </GlobalContext.Provider>
